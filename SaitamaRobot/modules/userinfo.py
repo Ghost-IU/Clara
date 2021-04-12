123ab244754e4cbc7184d8a214903bc33d99b2e9
Ghost-IU <Ghost-IU@users.noreply.github.com>
2021-04-12 11:19:44 +0000

    Format code using black

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -22,7 +22,7 @@ from SaitamaRobot import (
     WOLVES,
     INFOPIC,
     dispatcher,
-   )
+)
 from SaitamaRobot.__main__ import STATS, TOKEN, USER_INFO
 import SaitamaRobot.modules.sql.userinfo_sql as sql
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
